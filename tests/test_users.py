39a26de6b1cea9a9ac73c2b9ba72493f9e52bde1
zoeharris168-cell <zoeharris168@gmail.com>
2025-10-02 15:01:12 +0000

    add

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,42 +1,7 @@
-from fastapi.testclient import TestClient
-import pytest
 
-from app.database import get_db
-from app.main import app
+
 from app import schemas
-from app.models import Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
-
-
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
-def override_get_db():
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
-    
-
-
+from .database import client, session
 def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Hello world!"
@@ -50,3 +15,12 @@ def test_create_user(client):
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "testcreateuser@gmail.com"
     assert res.status_code == 201
+
+def test_login(client):
+    res = client.post(
+        '/login', data={
+            'username': 'testcreateuser@gmail.com',
+            "password": "password123"
+        }
+    )
+    assert res.status_code
